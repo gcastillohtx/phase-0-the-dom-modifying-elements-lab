64e1edddd85ae159e547b7e3b28729b3ff096e89
Gil <gcastillohtx@gmail.com>
2021-10-24 12:33:13 -0500

    Completed ASssignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//1
+main.remove();
+
+const newHeader= document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+
+document.getElementById("victory").append("Gil is the champion");
